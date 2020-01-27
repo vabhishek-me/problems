6af1fc524741b49240d2ae99b475f51bcfb48d83
abhishek <abhishek.k.v76076@gmail.com>
2020-01-27 13:09:19 +0530

    Update solution.js

--- daily-coding-problem/342-[Medium]-custom-reduce-function/solution.js
+++ daily-coding-problem/342-[Medium]-custom-reduce-function/solution.js
@@ -1,6 +1,6 @@
 const words = ["Lorem","ipsum","dolor","sit","amet","consectetur","adipiscing","elit","Suspendisse","fermentum","eros","sed","malesuada","faucibus","nibh","augue","tincidunt","risus","eget","pretium","sem","erat","non","metus","Aliquam","erat","volutpat","Nullam","vitae","arcu","placerat","tincidunt","felis","et","venenatis","sapien","Praesent","egestas","arcu","nec","faucibus","aliquet","Pellentesque","consequat","mauris","sit","amet","lectus","efficitur","ut","tristique","ligula","blandit","Mauris","pellentesque","euismod","tortor","sed","fermentum","Aliquam","suscipit","orci","quis","magna","egestas","malesuada","fermentum","tortor","interdum","Suspendisse","sit","amet","ornare","orci","Mauris","dapibus","erat","ut","aliquam","tincidunt","Sed","tempor","massa","ut","commodo","pretium","Aenean","ullamcorper","eget","nisi","eu","ultricies","Integer","pretium","ultricies","mauris","ultricies","aliquam","Quisque","vel","pretium","felis","Nam","neque","leo","dictum","sed","nunc","non","semper","pretium","elit"];
 
-// In built reduce
+// reduce built-in
 console.time("builtInReduce");
 console.log(words.reduce((acc, val) => {
 	acc[val] = (acc[val] || 0) + 1;
@@ -8,7 +8,7 @@ console.log(words.reduce((acc, val) => {
 }, {}));
 console.timeEnd("builtInReduce");
 
-// Recursive Reduce
+// Reduce using recursion
 const rreduce = (list, fn, acc) => {
 	if (list.length === 0) return acc;
   const [first, ...rest] = list;
@@ -22,7 +22,7 @@ console.log(rreduce(words, (acc, val) => {
 }, {}));
 console.timeEnd("recursiveReduce");
 
-// Loop Reduce
+// Reduce using loop
 const lreduce = (list, fn, acc) => {
 	let res = acc;
   for (let i=0; i < list.length; i++) {
