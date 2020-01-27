66992cf42d8b611ede72bf7ae3083b629199a750
abhishek <abhishek.k.v76076@gmail.com>
2020-01-27 13:09:59 +0530

    Update solution.js

--- daily-coding-problem/342-[Medium]-custom-reduce-function/solution.js
+++ daily-coding-problem/342-[Medium]-custom-reduce-function/solution.js
@@ -3,36 +3,36 @@ const words = ["Lorem","ipsum","dolor","sit","amet","consectetur","adipiscing","
 // reduce built-in
 console.time("builtInReduce");
 console.log(words.reduce((acc, val) => {
-	acc[val] = (acc[val] || 0) + 1;
+  acc[val] = (acc[val] || 0) + 1;
   return acc;
 }, {}));
 console.timeEnd("builtInReduce");
 
 // Reduce using recursion
 const rreduce = (list, fn, acc) => {
-	if (list.length === 0) return acc;
+  if (list.length === 0) return acc;
   const [first, ...rest] = list;
   const newAcc = fn(acc, first);
   return rreduce(rest, fn, newAcc);
 };
 console.time("recursiveReduce");
 console.log(rreduce(words, (acc, val) => {
-	acc[val] = (acc[val] || 0) + 1;
+  acc[val] = (acc[val] || 0) + 1;
   return acc;
 }, {}));
 console.timeEnd("recursiveReduce");
 
 // Reduce using loop
 const lreduce = (list, fn, acc) => {
-	let res = acc;
+  let res = acc;
   for (let i=0; i < list.length; i++) {
-  	res = fn(res, list[i]);
+    res = fn(res, list[i]);
   };
   return res;
 };
 console.time("loopReduce");
 console.log(lreduce(words, (acc, val) => {
-	acc[val] = (acc[val] || 0) + 1;
+  acc[val] = (acc[val] || 0) + 1;
   return acc;
 }, {}));
 console.timeEnd("loopReduce");
